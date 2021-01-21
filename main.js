16ae950d5c439edc59164eed6fdc66e3c1cb7b82
langesmartyna <langesmartyna@gmail.com>
2021-01-21 22:22:46 +0100

    zapis pierwszy

---
+++ main.js
@@ -0,0 +1,5 @@
+let myHeading = document.querySelector("p");
+
+console.log(myHeading);
+
+myHeading.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sunt corrupti repellat vero tenetur asperiores animi nulla beatae ipsa quae? dodaje dowolny tekst dzięki właściwościom text Content"
